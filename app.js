88a2eec6a3d044d66ad743227fff5d3731be904b
Janett <janettxgonzalez@gmail.com>
2018-06-29 02:40:26 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12deployed");
+mongoose.connect("mongodb://janettxg:westworld1@ds121321.mlab.com:21321/yelpcampxjanettxg");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
