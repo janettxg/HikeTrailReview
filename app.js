a4a88eb80ae1acaf4c86897add65307ab29c6416
janett g <janettxgonzalez@gmail.com>
2018-07-19 20:34:45 -0400

    update

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ app.use("/", indexRoutes);
 app.use("/trails", trailRoutes);
 app.use("/trails/:id/comments", commentRoutes);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8081;
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(port, process.env.IP, function(){
    console.log("The Hiking Trail Review Server Has Started!");
    console.log("port:" + port);
 });
